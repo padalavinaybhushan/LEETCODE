531e110e1579153538677e0558e8542c43a9b636
padalavinaybhushan <72262126+padalavinaybhushan@users.noreply.github.com>
2022-04-05 12:02:45 +0530

    Time: 108 ms (14.30%), Space: 125.4 MB (9.70%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static int fun(int[] prices,int inde ,int statee,int cape,int[][][] dp)
+    {
+        if(cape == 0) return 0;
+        if(inde == prices.length ) return 0;
+        // if(dp[ind][state][cap] != 0)
+        // {
+        //     return dp[ind][state][cap];
+        // }
+        for(int ind =prices.length-1;ind>=0;ind--){
+            for(int state=0;state<=1;state++){
+                for(int cap= 2;cap>0;cap--)
+                {
+                    if(state == 0)
+                    {
+                         int op1 = -1*prices[ind] + dp[ind+1][1][cap];
+                            int op2 =  dp[ind+1][state][cap];
+                           dp[ind][state][cap] = Math.max(op1,op2);
+                    }
+                    else
+                    {
+                        int op1 = prices[ind] +  dp[ind+1][0][cap-1];
+                        int op2 = dp[ind+1][state][cap];
+                         dp[ind][state][cap] = Math.max(op1,op2);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
+    }
+    public int maxProfit(int[] prices) {
+        int[][][] dp = new int[prices.length+1][2][3];
+        return fun(prices,0,0,2,dp);
+    }
+}
