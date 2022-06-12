2091980a07643777233f5b22f91d0dd3c8b1ce19
padalavinaybhushan <72262126+padalavinaybhushan@users.noreply.github.com>
2022-06-12 22:29:13 +0530

    Time: 131 ms (24.78%), Space: 120.4 MB (46.93%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,17 +1,35 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-	Map<Integer, Integer> lastIndex = new HashMap<>();
-	int[] prefixSum = new int[nums.length + 1];
-
-	int maxScore = 0;
-	for (int l=0, r=0; r<nums.length; r++) {
-		prefixSum[r+1] = prefixSum[r] + nums[r];
-		if (lastIndex.containsKey(nums[r])) 
-			l = Math.max(l, lastIndex.get(nums[r]) + 1);
-		maxScore = Math.max(maxScore, prefixSum[r+1] - prefixSum[l]);
-		lastIndex.put(nums[r], r);
-	}
-
-	return maxScore;
-}
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int len = nums.length,ans = Integer.MIN_VALUE;
+        int sum = nums[0];
+        int[] pre = new int[len];
+        pre[0]=nums[0];
+        for(int i=1;i<len;i++)
+        {
+            pre[i] = nums[i]+ pre[i-1];
+        }
+        hm.put(nums[0],0);
+        int start=0;
+        //System.out.println(Arrays.toString(pre));
+        for(int i=1;i<len;i++)
+        {
+            if(hm.containsKey(nums[i]) && hm.get(nums[i])>=start)
+            {
+                sum = pre[i]-pre[hm.get(nums[i])];
+                start=hm.get(nums[i])+1;
+            }
+            else
+            {
+                sum+=nums[i];
+                
+            }
+            ans = Math.max(ans,sum);
+            //System.out.println(ans+" "+sum);
+            hm.put(nums[i],i);
+            nums[i] += nums[i-1];
+            
+        }
+        return Math.max(sum,ans);
+    }
 }
