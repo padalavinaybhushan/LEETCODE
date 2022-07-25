a471f11354042251c22833a794250d5e82770347
padalavinaybhushan <72262126+padalavinaybhushan@users.noreply.github.com>
2022-07-25 10:40:30 +0530

    Time: 1 ms (38.49%), Space: 46.7 MB (79.79%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,11 +1,11 @@
 class Solution {
-    public static int floor(int[] arr,int tar,int i,int j){
+    public static int floor(int[] arr,int tar,int i,int j,boolean flag){
         int low=i,high=j,ans=-1;
         while(low<=high){
             int mid = low+(high-low)/2;
             if(arr[mid] == tar){
                 ans=mid;
-                high=mid-1;
+                if(flag) high=mid-1; else low=mid+1;
             }
             else if(arr[mid] < tar){
                 low=mid+1;
@@ -16,27 +16,27 @@ class Solution {
         }
         return ans;
     }
-    public static int ceil(int[] arr,int tar,int i,int j){
-        int low=i,high=j,ans=-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(arr[mid] == tar){
-                ans=mid;
-                low=mid+1;
-            }
-            else if(arr[mid] < tar){
-                low=mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        return ans;
-    }
+    // public static int ceil(int[] arr,int tar,int i,int j){
+    //     int low=i,high=j,ans=-1;
+    //     while(low<=high){
+    //         int mid = low+(high-low)/2;
+    //         if(arr[mid] == tar){
+    //             ans=mid;
+    //             low=mid+1;
+    //         }
+    //         else if(arr[mid] < tar){
+    //             low=mid+1;
+    //         }
+    //         else{
+    //             high = mid-1;
+    //         }
+    //     }
+    //     return ans;
+    // }
     public int[] searchRange(int[] nums, int target) {
         int ans[] = new int[2];
-        ans[0]=floor(nums,target,0,nums.length-1);
-        ans[1]=ceil(nums,target,0,nums.length-1);
+        ans[0]=floor(nums,target,0,nums.length-1,true);
+        ans[1]=floor(nums,target,0,nums.length-1,false);
         return ans;
     }
 }
