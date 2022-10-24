8b392b12e51539b9a7622508af725869a8e8882b
padalavinaybhushan <72262126+padalavinaybhushan@users.noreply.github.com>
2022-10-24 21:50:13 +0530

    Time: 20 ms (76.91%), Space: 42.6 MB (79.89%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,43 @@
+class Solution {
+    public static int count(int n){
+        int ans = 0;
+        while(n!=0){
+            ans++;
+            n = n&(n-1);
+        }
+        return ans;
+    }
+    public static void solve(ArrayList<Integer> val,ArrayList<Integer> a,int ind,int prev,int n){
+        if(ind == n) {
+            val.add(count(prev));
+            return ;
+        }
+        if(count(prev)+count(a.get(ind))==(count(prev|a.get(ind)))){
+            solve(val,a,ind+1,prev^a.get(ind),n);
+        }
+        solve(val,a,ind+1,prev,n);
+    }
+    public int maxLength(List<String> arr) {
+        int n = arr.size();
+        ArrayList<Integer> a = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            String str = arr.get(i);
+            int t = 0;
+            int len =str.length();
+            int ans=0;
+            for(int j=0;j<len;j++){
+                ans = ans|(1<<(int)(str.charAt(j)-'a'+1));
+            }
+           // System.out.println(len+" "+str);
+            if(count(ans) == len)
+                a.add(ans);
+        }
+        int size = a.size();
+        //System.out.println(a);
+        ArrayList<Integer> val = new ArrayList<>();
+        solve(val,a,0,0,size);
+        val.add(0);
+        //System.out.println(val);
+        return Collections.max(val);
+    }
+}
